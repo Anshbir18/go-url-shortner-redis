e30f16a95a9e7e45d0d090e5387803bfe784c0ae
Anshbir-Singh <anshbir181@gmail.com>
2026-02-15 19:22:04 +0530

    part4

--- api/routes/shorten_url.go
+++ api/routes/shorten_url.go
@@ -2,16 +2,19 @@ package routes
 
 import (
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/Anshbir18/go-url-shortner-redis/database"
 	"github.com/Anshbir18/go-url-shortner-redis/helpers"
 	"github.com/asaskevich/govalidator"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
 // define request
 type request struct {
-	URL         string        `json:"url`
+	URL         string        `json:"url"`
 	CustomShort string        `json:"short"`
 	Expiry      time.Duration `json:"expiry"`
 }
@@ -33,20 +36,59 @@ func ShortenURL(c *gin.Context) {
 		return
 	}
 
-	// implement rate limiting // we want a user to only be able to make 10 requests per 30 minutes if they exceed that we want to return an error message
+	// ---------------- RATE LIMITING ----------------
 
-	//check if the input is valid
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	ip := c.ClientIP()
+
+	val, err := r2.Get(database.Ctx, ip).Result()
+
+	if err == redis.Nil {
+		// first request from this IP
+		if err := r2.Set(database.Ctx, ip, os.Getenv("API_QUOTA"), 30*time.Minute).Err(); err != nil {
+			c.JSON(500, gin.H{"error": "internal server error"})
+			return
+		}
+	} else if err != nil {
+		c.JSON(500, gin.H{"error": "redis error"})
+		return
+	} else {
+		valInt, convErr := r2.Get(database.Ctx, ip).Int()
+		if convErr != nil {
+			c.JSON(500, gin.H{"error": "redis parse error"})
+			return
+		}
+
+		if valInt <= 0 {
+			ttl, _ := r2.TTL(database.Ctx, ip).Result()
+
+			c.JSON(429, gin.H{
+				"error":            "rate limit exceeded",
+				"rate_limit_reset": ttl / time.Minute,
+			})
+			return
+		}
+	}
+
+	// ---------------- VALIDATIONS ----------------
 
 	if !govalidator.IsURL(body.URL) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid URL"})
+		return
 	}
-
-	//check for domain erros
 
 	if !helpers.RemoveDomainError(body.URL) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "you cannot shorten the domain itself"})
+		return
 	}
 
-	//enforece https ssl
+	// enforce https
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	// decrement quota
+	_ = r2.Decr(database.Ctx, ip).Err()
+
+	// -------- your remaining logic continues below --------
 }
